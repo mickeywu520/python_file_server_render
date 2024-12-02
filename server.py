67a570073b24c2a26939ffb4ae4483b438e3e0e4
mickey[吳亞哲] <mickey@arbor.com.tw>
2024-12-02 16:38:47 +0800

    add new api

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, jsonify, request
 import os
 
 app = Flask(__name__)
@@ -10,16 +10,29 @@ def download_file(filename):
     except FileNotFoundError:
         return "File not found", 404
 
+@app.route('/list-files')
+def list_files():
+    try:
+        files = os.listdir('hex_files')
+        hex_files = [f for f in files if f.endswith('.hex')]
+        return jsonify(hex_files=hex_files)
+    except FileNotFoundError:
+        return "hex_files directory not found", 404
+
 @app.route('/')
 def index():
     return """
     <html>
     <body>
         <h1>Firmware Download Server</h1>
-        <a href="/download/test_firmware.hex">Download Test Firmware</a>
+        <a href="/list-files">List Available Firmware Files</a>
     </body>
     </html>
     """
+
+@app.route('/hello')
+def hello():
+    return jsonify(message="hello world")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
